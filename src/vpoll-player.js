1df64a7d835079fed82ecf3cbe6234743e204897
Krystian Hoffman <krystian.hoffman@mtgx.se>
2015-06-10 16:11:09 +0200

    wrapped videojs in vPollPlayer

--- src/vpoll-player.js
+++ src/vpoll-player.js
@@ -2,3 +2,14 @@ use 'strict';
 
 let vjs = require('video.js');
 
+window.vjs = window.vjs || vjs;
+window.videojs = window.videojs || vjs;
+
+module.exports = function vPollPlayer(elementId, options) {
+
+	var player = vjs(elementId, options);
+
+
+	return player;
+
+};
