56e3c3480ee7465cd531b916069676c7b7c58917
Marcus Öhman <marcus.ohman@mtgx.se>
2015-06-12 15:21:38 +0200

    chart layout

--- src/vpoll-player.js
+++ src/vpoll-player.js
@@ -12,6 +12,28 @@ window.vjs = window.vjs || vjs;
 window.videojs = window.videojs || vjs;
 window.vPollPlayer = vPollPlayer;
 
+//
+window.chartdata = {
+    labels: ["Petter","Ashkan"],
+    datasets: [
+        {
+            label: "Vem vinner matchen?",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59]
+        }
+    ]
+};
+window.chartoptions = [];
+chart.defaults.global.scaleBeginAtZero = true;
+chart.defaults.global.scaleSteps = 5;
+chart.defaults.global.scaleStepWidth = 20;
+chart.defaults.global.scaleStartValue = 0;
+chart.defaults.global.scaleOverride = true;
+chart.defaults.global.scaleLabel = "<%=value%>%";
+//
 
 export default function vPollPlayer(elementId, options) {
   if (!options.socketUrl) throw new Error('options.socketUrl needs to be specified');
@@ -24,6 +46,11 @@ export default function vPollPlayer(elementId, options) {
 
   let chartComponent = new vjs.Chart( player, options );
   let chartEl = player.addChild(chartComponent);
+
+  var ctx = document.getElementById("myChart").getContext("2d");
+  window.barChart = new chart(ctx).Bar(window.chartdata, window.chartoptions);
 	
   return player;
 };
+
+
